241f9f5243eae1c73cdb431055ba0193e7d668bd
Jack Meier <jackson.meier423@gmail.com>
2020-07-12 20:45:01 -0700

    added / route

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -7,6 +7,10 @@ var PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../index.html"));
+  });
+
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../../notes.html"));
   });
